8ac5e66fbd13f72442a16db665d4b9432ed708b5
GuilhermeFerreiraDS <140648348+GuilhermeFerreiraDS@users.noreply.github.com>
2025-12-03 21:17:54 +0000

    mais dois erros

--- src/screens/HomeStyle.tsx
+++ src/screens/HomeStyle.tsx
@@ -1,16 +1,41 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
   },
-  botao:{
-    backgroundColor:'#000',
-    padding:15
+  cardLogo: {
+    width: 200,
+    height: 180,
+    borderWidth: 1,
+    borderColor: "#aaa",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 40,
   },
-  textoBotao:{
-    color:'#fff'
-  }
+  logo: {
+    width: 130,
+    height: 130,
+  },
+  button: {
+    backgroundColor: "#3d3534",
+    width: 220,
+    paddingVertical: 12,
+    borderRadius: 6,
+    marginBottom: 15,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  link: {
+    marginTop: 10,
+    color: "#333",
+  },
 });
